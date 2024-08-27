eccd26276651eccfa8360be320dfc5dc6e8a30c0
Huy Doan <doanhuy86@gmail.com>
2024-08-27 11:19:04 -0400

    initial commit

---
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -0,0 +1,122 @@
+package com.udacity.webcrawler;
+
+import com.udacity.webcrawler.json.CrawlResult;
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
+import org.checkerframework.checker.units.qual.C;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * A concrete implementation of {@link WebCrawler} that runs multiple threads on a
+ * {@link ForkJoinPool} to fetch and process multiple web pages in parallel.
+ */
+final class ParallelWebCrawler implements WebCrawler {
+  private final Clock clock;
+  private final Duration timeout;
+  private final int popularWordCount;
+  private final ForkJoinPool pool;
+  private final List<Pattern> ignoredUrls;
+  private final int maxDepth;
+  private final PageParserFactory parserFactory;
+  @Inject
+  ParallelWebCrawler(
+      Clock clock,
+      @Timeout Duration timeout,
+      PageParserFactory parserFactory,
+      @PopularWordCount int popularWordCount,
+      @TargetParallelism int threadCount,
+      @IgnoredUrls List<Pattern> ignoredUrls,
+      @MaxDepth int maxDepth) {
+    this.clock = clock;
+    this.timeout = timeout;
+    this.parserFactory = parserFactory;
+    this.popularWordCount = popularWordCount;
+    this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
+    this.ignoredUrls = ignoredUrls;
+    this.maxDepth = maxDepth;
+  }
+
+  @Override
+  public CrawlResult crawl(List<String> startingUrls) {
+    Instant deadline = clock.instant().plus(timeout);
+    Map<String, Integer> counts = new HashMap<>();
+    Set<String> visitedUrls = new HashSet<>();
+    for (String url : startingUrls) {
+      pool.invoke(new CrawlTask(url, deadline, maxDepth, counts, visitedUrls));
+    }
+
+    if (counts.isEmpty()) {
+      return new CrawlResult.Builder()
+              .setWordCounts(counts)
+              .setUrlsVisited(visitedUrls.size())
+              .build();
+    }
+
+    return new CrawlResult.Builder()
+            .setWordCounts(WordCounts.sort(counts, popularWordCount))
+            .setUrlsVisited(visitedUrls.size())
+            .build();
+  }
+
+  @Override
+  public int getMaxParallelism() {
+    return Runtime.getRuntime().availableProcessors();
+  }
+
+  public class CrawlTask extends RecursiveAction {
+    private String url;
+    private Instant deadline;
+    private int maxDepth;
+    private Map<String, Integer> counts;
+    private Set<String> visitedUrls;
+
+    public CrawlTask (
+            String url,
+            Instant deadline,
+            int maxDepth,
+            Map<String, Integer> counts,
+            Set<String> visitedUrls) {
+      this.url = url;
+      this.deadline = deadline;
+      this.maxDepth = maxDepth;
+      this.counts = counts;
+      this.visitedUrls = visitedUrls;
+    }
+
+    @Override
+    protected void compute() {
+      if (maxDepth == 0 || clock.instant().isAfter(deadline)) {
+        return;
+      }
+      for (Pattern pattern : ignoredUrls) {
+        if (pattern.matcher(url).matches()) {
+          return;
+        }
+      }
+      if (visitedUrls.contains(url)) {
+        return;
+      }
+      visitedUrls.add(url);
+      PageParser.Result result = parserFactory.get(url).parse();
+      for (Map.Entry<String, Integer> entry : result.getWordCounts().entrySet()) {
+        counts.compute(entry.getKey(), (k, v) -> (v == null ) ? entry.getValue() : entry.getValue() + v);
+      }
+      List<CrawlTask> subtasks = result.getLinks()
+              .stream()
+              .map(link -> new CrawlTask(link,deadline,maxDepth - 1, counts, visitedUrls))
+              .toList();
+      invokeAll(subtasks);
+    }
+  }
+}
---
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -0,0 +1,74 @@
+package com.udacity.webcrawler.profiler;
+
+import javax.inject.Inject;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Proxy;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.Clock;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
+
+/**
+ * Concrete implementation of the {@link Profiler}.
+ */
+final class ProfilerImpl implements Profiler {
+
+  private final Clock clock;
+  private final ProfilingState state = new ProfilingState();
+  private final ZonedDateTime startTime;
+
+  @Inject
+  ProfilerImpl(Clock clock) {
+    this.clock = Objects.requireNonNull(clock);
+    this.startTime = ZonedDateTime.now(clock);
+  }
+
+  @Override
+  public <T> T wrap(Class<T> klass, T delegate) {
+    Objects.requireNonNull(klass);
+
+    // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
+    //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
+    //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
+
+    if (delegate.getClass().getDeclaredMethods().length == 0 ||
+            Arrays.stream(delegate.getClass()
+                    .getDeclaredMethods())
+                    .anyMatch(method -> method.getAnnotation(Profiled.class) != null)){
+      throw new IllegalArgumentException("No annotated methods");
+    }
+    else {
+      return (T) Proxy.newProxyInstance(
+              klass.getClassLoader(),
+              new Class<?>[]{klass},
+              new ProfilingMethodInterceptor(this.clock,  delegate, state));
+    }
+  }
+
+  @Override
+  public void writeData(Path path) throws IOException {
+    // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
+    //       path, the new data should be appended to the existing file.
+    try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.APPEND, StandardOpenOption.CREATE)) {
+      writeData(writer);
+    }
+  }
+
+  @Override
+  public void writeData(Writer writer) throws IOException {
+    writer.write("Run at " + RFC_1123_DATE_TIME.format(startTime));
+    writer.write(System.lineSeparator());
+    state.write(writer);
+    writer.write(System.lineSeparator());
+  }
+}
